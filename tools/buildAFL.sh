1fb6dcfcb4daec454f553e86c5da00e782634c0d
root <root@ubuntu18.myguest.virtualbox.org>
2024-09-16 19:58:06 -0600

    Added path updates to buildAFL.sh

--- tools/buildAFL.sh
+++ tools/buildAFL.sh
@@ -8,4 +8,12 @@ make clean && make
 cd /home/security/EDJ-DynamicGen-CS5890-6890/AFLplusplus/
 make source-only
 
+# Update environment variables
+NEW_PATH="PATH=\$PATH:/home/security/EDJ-DynamicGen-CS5890-6890/AFLplusplus/"
+if ! grep -q "$NEW_PATH" ~/.bashrc; then
+echo "export $NEW_PATH" | tee -a ~/.bashrc > /dev/null
+else
+echo "Path has already been updated"
+fi
+
 
